ecdf036bccb54d2de61312d5c3b71fd5ae6c9f06
Isabella Crasto <crasto@usc.edu>
2026-04-20 12:29:17 -0700

    Fixed auth branch compatibility and migration version conflict

--- src/main/java/com/csci201/backend/repository/UserRepository.java
+++ src/main/java/com/csci201/backend/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.csci201.backend.repository;
 
 import com.csci201.backend.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+}
